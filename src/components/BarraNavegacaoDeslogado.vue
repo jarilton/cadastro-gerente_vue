1df842f6598d0873e4b7dbd4df720ef52dd5a4a9
Jarilton Junior Silva Pereira <jariltonpereira02@hotmail.com>
2022-06-06 22:54:43 -0300

    tratando a barra de navegação

---
+++ src/components/BarraNavegacaoDeslogado.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="BarraNavegacaoDeslogado">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/login">Login</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/novoUsuario" class="nav-link"> Registre-se </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BarraNavegacaoDeslogado",
+};
+</script>
+
+<style></style>
